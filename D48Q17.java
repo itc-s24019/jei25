710c385f307534589079c555a14db729a3f62906
itc-s24019 <s24019@std.it-college.ac.jp>
2025-05-16 11:35:58 +0900

    課題

---
+++ D48Q17.java
@@ -0,0 +1,13 @@
+public class D48Q17 {
+    public static void main(String[] args) {
+        int n = 35;
+        System.out.println(n > 15 && n <= 30);
+
+    }
+}
+
+/*
+D48Q17について
+D48は「第48回」のことを表しています
+
+*/
