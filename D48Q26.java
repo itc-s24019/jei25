80f7a2f8c8b2b869ef4db379bedbcd0bd9cc91cc
itc-s24019 <s24019@std.it-college.ac.jp>
2025-05-16 12:39:45 +0900

    課題

---
+++ D48Q26.java
@@ -0,0 +1,9 @@
+public class D48Q26 {
+    public static  void  main(String[] arg){
+        int i = 0, sum = 0;
+        while (++i < 8) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
