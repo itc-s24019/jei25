0038a98cfd1bb6a850000e59c38a6f1d21f9bf3b
itc-s24019 <s24019@std.it-college.ac.jp>
2025-05-23 11:59:31 +0900

    課題提出

---
+++ D49Q21.java
@@ -0,0 +1,15 @@
+public class D49Q21 {
+    public static void main(String[] args) {
+
+        int n1 = 5;
+        int n2 = 0x10;
+
+        System.out.println(n1 * n2); // 5 * 16 = 80
+    }
+}
+    /*
+    コードの修正をしたいのですが、
+    コンパイル及び実行され,「80」が表示されるようにコードを
+    修正してみてください。
+    (64+16=80)
+    */
