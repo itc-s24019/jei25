8a4b4b38319141f7b41f0a7b658922a1530d4e61
itc-s24019 <s24019@std.it-college.ac.jp>
2025-06-24 10:15:27 +0900

    課題提出

---
+++ D50Q25.java
@@ -0,0 +1,9 @@
+public class D50Q25 {
+
+    public static void main(String[] args) {
+        for (int i = 2; i <= 8; i +=2) {
+            if (i % 2 == 0)
+            System.out.println(i);
+        }
+    }
+}
