bb7c19cc60c14c3717d9cc790123461ec94c6aed
itc-s24019 <s24019@std.it-college.ac.jp>
2025-08-01 10:18:29 +0900

    課題提出

---
+++ D53Q20.java
@@ -0,0 +1,9 @@
+public class D53Q20 {
+    public  static void main(String[] args) {
+        int i = 8;
+        float f = 3.27f;
+
+        int n = i + (int)f;
+        System.out.println(n);
+    }
+}
