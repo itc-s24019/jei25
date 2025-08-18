d3d3e4628c115250f6a8418958eb0c75de0edc4e
itc-s24019 <s24019@std.it-college.ac.jp>
2025-08-18 12:39:41 +0900

    課題提出

---
+++ D53Q21kadai.java
@@ -0,0 +1,15 @@
+public class D53Q21kadai {
+    public static void main(String[] args) {
+
+        byte a = 0b111111;
+
+        byte b = 0100;
+
+        byte c = 0x41;
+
+
+        System.out.println("変数 a の値: " + a); // -> 63
+        System.out.println("変数 b の値: " + b); // -> 64
+        System.out.println("変数 c の値: " + c); // -> 65
+    }
+}
